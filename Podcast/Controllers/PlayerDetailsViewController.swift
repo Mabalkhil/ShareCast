f6d35218ef7ae0b0ae7d55d15cb19fffa8a9dace
khaled <khaled.hm@outlook.com>
2019-02-05 01:02:37 +0300

    The new Player added

--- Podcast/Controllers/PlayerDetailsViewController.swift
+++ Podcast/Controllers/PlayerDetailsViewController.swift
@@ -12,7 +12,7 @@ import ACBAVPlayer
 
 
 
-class PlayerDetailsViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
+class PlayerDetailsViewController: UIViewController {
 
     
     @IBOutlet weak var scrollView: UIScrollView!
@@ -40,18 +40,12 @@ class PlayerDetailsViewController: UIViewController, UIScrollViewDelegate, UITab
         return img
     }()
     
-    var noMarksText: UITextView = {
-        let text = UITextView()
-        text.textColor = .white
-        
-        text.backgroundColor = #colorLiteral(red: 0.2548794746, green: 0.254914552, blue: 0.2548675537, alpha: 1)
-        return text
-    }()
-    
     struct Mark {
         var time:String
         var desc:String
     }
+    
+    var marks: [Mark] = []
     
     var episode:Episode!
     
@@ -68,6 +62,7 @@ class PlayerDetailsViewController: UIViewController, UIScrollViewDelegate, UITab
         
         //Innitializing episode and start playing it
         episodeName?.text = episode.title
+        channelName.text = episode.author
         playEpisode()
         guard let url = URL(string: episode.imageUrl ?? "") else { return }
         episodeImg.sd_setImage(with: url)
@@ -76,92 +71,7 @@ class PlayerDetailsViewController: UIViewController, UIScrollViewDelegate, UITab
         
     }
     
-    //MARK:- Scroll View
-    //change the pageview when scrolled
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageNumber = scrollView.contentOffset.x / scrollView.frame.size.width
-        pageControl.currentPage = Int(pageNumber)
-    }
     
-    //setting scroll view
-    func setScrollView() {
-        pageControl.numberOfPages = 2
-        
-        frame.origin.x = scrollView.frame.size.width * 0
-        frame.size = scrollView.frame.size
-        
-        episodeImg.frame = frame
-        scrollView.addSubview(episodeImg)
-        handleTimeMark()
-        
-        scrollView.contentSize = CGSize(width: scrollView.frame.size.width * 2 + 2, height: scrollView.frame.size.height)
-        scrollView.delegate = self
-        //scrollView.isScrollEnabled = false
-    }
-    
-    
-    //MARK:- Table view functions
-    
-    var marks: [Mark] = []
-    
-    func setTable() {
-        
-        frame.origin.x = scrollView.frame.size.width + 5
-        //frame.size = scrollView.frame.size
-        
-        myTableView = UITableView(frame: frame)
-        myTableView.backgroundColor = #colorLiteral(red: 0.2548794746, green: 0.254914552, blue: 0.2548675537, alpha: 1)
-        myTableView.delegate = self
-        myTableView.dataSource = self
-        myTableView.register(TimeMarkCell.self, forCellReuseIdentifier: "TimeMarkCell")
-        
-        scrollView.addSubview(myTableView)
-    }
-    
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.marks.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeMarkCell", for: indexPath) as! TimeMarkCell
-        let tm = self.marks[indexPath.row]
-        cell.time = tm.time
-        cell.desc = tm.desc
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cell.backgroundColor = #colorLiteral(red: 0.2548794746, green: 0.254914552, blue: 0.2548675537, alpha: 1)
-    }
-    
-    //Handing the selected time mark
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let selectedTime = self.marks[indexPath.row].time
-        
-        let timeInSecondsList = selectedTime.split(whereSeparator: { $0 == ":" || $0 == " " })//.map { Double($0)!}
-        var timeInSeconds:Double = 0
-        if (timeInSecondsList.count == 3 ) {
-            let hours = (Double(timeInSecondsList[0]) ?? 0.0 ) * 3600
-            let minutes = (Double(timeInSecondsList[1])  ?? 0.0) * 60
-            let seconds = (Double( timeInSecondsList[2])  ?? 0.0)
-            timeInSeconds = hours + minutes + seconds
-                        }
-        else {
-            let minutes = (Double(timeInSecondsList[0])  ?? 0.0) * 60
-            let seconds = (Double( timeInSecondsList[1])  ?? 0.0)
-            timeInSeconds =  minutes + seconds
-                    }
-        
-        self.player.seek(to: CMTimeMakeWithSeconds(timeInSeconds, preferredTimescale: 1))
-        
-    }
     //##################################################################################################
     
     //MARK:- Showing Progress
@@ -370,11 +280,8 @@ class PlayerDetailsViewController: UIViewController, UIScrollViewDelegate, UITab
             setTable()
         }
         else {
-            frame.origin.x = scrollView.frame.size.width + 5
-            noMarksText.frame = frame
-            noMarksText.text = "No time marks for this episode"
-            noMarksText.textAlignment = .center
-            scrollView.addSubview(noMarksText)
+            scrollView.isScrollEnabled = false
+            pageControl.numberOfPages = 1
         }
         
     }
